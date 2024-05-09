fdb31106d226d68949054ebbaff099a6c83fa6d5
Corner <wall.corner@outlook.com>
2024-05-09 22:05:37 +0800

    release: 1.1.11

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -3,7 +3,12 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
+    id("dev.hydraulic.conveyor") version "1.9"
 }
+
+version = 1
+group = "TV-Multiplatform"
+
 
 kotlin {
     jvm()
@@ -32,10 +37,21 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Pkg)
             packageName = "TV"
-            packageVersion = "1.1.9"
+            packageVersion = "1.1.11"
             vendor = "TV Multiplatform"
 
-            modules("java.compiler", "java.instrument", "java.management", "java.naming", "java.net.http", "java.rmi", "java.security.jgss", "java.sql", "jdk.httpserver", "jdk.unsupported")
+            modules(
+                "java.compiler",
+                "java.instrument",
+                "java.management",
+                "java.naming",
+                "java.net.http",
+                "java.rmi",
+                "java.security.jgss",
+                "java.sql",
+                "jdk.httpserver",
+                "jdk.unsupported"
+            )
             appResourcesRootDir.set(project.layout.projectDirectory.dir("resources"))
 //            app icons https://github.com/JetBrains/compose-multiplatform/tree/master/tutorials/Native_distributions_and_local_execution#app-icon
             windows {
@@ -44,16 +60,20 @@ compose.desktop {
                 upgradeUuid = "161FA5A0-A30B-4568-9E84-B3CD637CC8FE"
             }
 
-            linux{
+            linux {
                 iconFile.set(project.file("src/jvmMain/resources/TV-icon-s.png"))
 
             }
 
-            macOS{
+            macOS {
                 iconFile.set(project.file("src/jvmMain/resources/icon.icns"))
             }
 
         }
 
     }
+}
+
+project.configurations.create("desktopRuntimeClasspath") {
+    extendsFrom(project.configurations.findByName("jvmRuntimeClasspath"))
 }
