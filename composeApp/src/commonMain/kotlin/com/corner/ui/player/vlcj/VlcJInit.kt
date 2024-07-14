cf14bdbd01cee40cbf676066f97b98129e2e67ae
Corner <wall.corner@outlook.com>
2024-07-14 16:06:14 +0800

    feat： 修改vlc组件加载提示

--- composeApp/src/commonMain/kotlin/com/corner/ui/player/vlcj/VlcJInit.kt
+++ composeApp/src/commonMain/kotlin/com/corner/ui/player/vlcj/VlcJInit.kt
@@ -1,5 +1,7 @@
 package com.corner.ui.player.vlcj
 
+import com.corner.bean.SettingStore
+import com.corner.ui.getPlayerSetting
 import com.corner.ui.scene.SnackBar
 import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery
 
@@ -16,7 +18,7 @@ class VlcJInit {
 //                println("VlcJInit 未找到${Constants.resPathKey}环境变量")
 //            }
             val discover = NativeDiscovery().discover()
-            if(!discover) SnackBar.postMsg("未找到VLC组件， 请安装VLC或者配置vlc可执行文件位置")
+            if(!discover && SettingStore.getPlayerSetting()[0] as Boolean) SnackBar.postMsg("未找到VLC组件， 请安装VLC或者配置vlc可执行文件位置")
         }
     }
 }
