6acb364d588680f1c3d4a40eb4e6a16b1a9f8037
clevebitr <97876278+clevebitr@users.noreply.github.com>
2025-07-21 21:37:57 +0800

    ApiConfig.kt : 添加超时时间，重写parseExt防止栈溢出 ChooseItem.kt : 重写load样式 DetailScreen.kt : 修复了选集按钮不高亮上次观看集数的按钮 DetailViewModel.kt : 添加记录上次选择按钮的变量 Http.kt : get函数允许接受自定义超时时间 libs.versions.toml : 更新版本号 SettingsScreen.kt : 更新版本号

--- composeApp/src/commonMain/kotlin/com/corner/catvodcore/util/Http.kt
+++ composeApp/src/commonMain/kotlin/com/corner/catvodcore/util/Http.kt
@@ -2,7 +2,6 @@ package com.corner.catvodcore.util
 
 import com.corner.util.KtorClient
 import com.github.catvod.bean.Doh
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import okhttp3.*
 import okhttp3.Credentials.basic
