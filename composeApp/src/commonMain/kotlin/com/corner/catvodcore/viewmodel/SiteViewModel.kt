34ea8d174008cd5a99987d9763bdde7634396c95
clevebitr <97876278+clevebitr@users.noreply.github.com>
2025-07-28 21:34:38 +0800

    Init.kt : 新增初始化广告过滤设置 LumenTV Proxy Placeholder Webpage.html : 新增代理默认界面 M3U8AdFilterInterceptor.kt : 新增广告过滤请求拦截器 M3U8Cache.kt : 新增M3U8文件缓存工具类 M3U8Filter.kt : 完善广告过滤类实现，来自ltxlong M3U8FilterConfig.kt : 新增广告过滤类使用的配置类 OkHttp.kt : 添加拦截器 Routings.kt : 添加缓存文件本地代理 Setting.kt : 添加广告过滤器配置相关函数 SettingsScreen.kt : 添加设置广告过滤器卡片 SiteViewModel.kt : 重构processM3U8方法 添加licenses相关文件

--- composeApp/src/commonMain/kotlin/com/corner/catvodcore/viewmodel/SiteViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/corner/catvodcore/viewmodel/SiteViewModel.kt
@@ -33,10 +33,8 @@ import java.util.concurrent.CopyOnWriteArrayList
 import com.corner.ui.nav.data.DialogState
 import com.corner.ui.nav.data.DialogState.toggleSpecialVideoLink
 import com.corner.util.M3U8AdFilterInterceptor
-import com.corner.util.M3U8FilterConfig
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.File
 
 object SiteViewModel {
     private val log = LoggerFactory.getLogger("SiteViewModel")
