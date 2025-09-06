2e5a88db5062a787f7f8102cd061e0f18f863880
clevebitr <97876278+clevebitr@users.noreply.github.com>
2025-09-06 17:16:51 +0800

    fix:导航栏 update:文件夹icon

--- composeApp/src/commonMain/kotlin/com/corner/ui/scene/BaseScene.kt
+++ composeApp/src/commonMain/kotlin/com/corner/ui/scene/BaseScene.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
