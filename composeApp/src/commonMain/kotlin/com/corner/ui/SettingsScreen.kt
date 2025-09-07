718823b59dfe470838bf42183269e66a9bdc5110
clevebitr <97876278+clevebitr@users.noreply.github.com>
2025-09-07 16:08:34 +0800

    update:颜色配置 update:version

--- composeApp/src/commonMain/kotlin/com/corner/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/corner/ui/SettingsScreen.kt
@@ -881,7 +881,7 @@ fun AboutDialog(
                         )
 
                         Text(
-                            text = "1.0.6",
+                            text = "1.0.7",
                             style = MaterialTheme.typography.bodySmall,
                             color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
                         )
